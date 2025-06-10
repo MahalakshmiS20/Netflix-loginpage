aa29e215b875177fb3e4f74b650715cacfed736e
MahalakshmiS20 <mahalakshmisenthil208@gmail.com>
2025-06-10 13:20:10 +0530

    files added

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const { urlencoded } = require("body-parser")
+
+var email = "maha@gmail.com"
+var password = "54321"
+
+app.use(express.json())
+app.use(cors())
+app.post('/login',function(req,res){
+   // console.log(req)
+   if(email===req.body.email& password===req.body.password)
+   {
+     res.send(true)
+   }
+   else
+   {
+    res.send(false)
+   }
+    
+   
+})
+
+app.listen(8000,function(){
+    console.log("server started")
+})
