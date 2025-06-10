043e8cdf8043c059c0e10bfc7b584a2b0906d770
MahalakshmiS20 <mahalakshmisenthil208@gmail.com>
2025-06-10 14:43:30 +0530

    updated

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express")
 const cors = require("cors")
+
 const app = express()
-const { urlencoded } = require("body-parser")
+
 
 var email = "maha@gmail.com"
 var password = "54321"
 
 app.use(express.json())
 app.use(cors())
+
 app.post('/login',function(req,res){
    // console.log(req)
-   if(email===req.body.email& password===req.body.password)
+   if(email===req.body.email && password===req.body.password)
    {
      res.send(true)
    }
